c3a8a30f49c71a7d7d2653257b6a294630e7b518
Israel192619 <israel.zc.09@gmail.com>
2022-06-27 08:23:55 -0500

    Ejercicio 4 recursividad

---
+++ src/Ejercicio_4.java
@@ -0,0 +1,15 @@
+public class Ejercicio_4 {
+    public static String invertirCadena(String cadena, int indice){
+        String inversa;
+
+        if (indice == 0){
+            inversa = String.valueOf(cadena.charAt(indice));
+        }else{
+            inversa = String.valueOf(cadena.charAt(indice))+invertirCadena(cadena,indice - 1);
+        }
+        return inversa;
+    }
+    public static String invertirCadena(String cadena){
+        return invertirCadena(cadena,cadena.length()-1);
+    }
+}
